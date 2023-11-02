a284bae3da2c40642d91446b24654a29596ebe4c
ROHIT LUDBE <rohitludbe@kccemsr.edu.in>
2023-11-02 15:59:38 +0530

    added favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,18 @@ module.exports = {
     title: `news-app`,
     siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: './src/images/news.svg'
+      },
+    },
+  ],
 }
